08a12883f266792edbf2eb4869c427dff43e02c7
Vedmaka <god.vedmaka@gmail.com>
2021-06-25 16:55:56 +0300

    Init
    Change-Id: Ied530ea707301ff5d343cb980e2193da97121cdc

---
+++ README.md
@@ -0,0 +1,33 @@
+# PerPageLanguage
+
+The extension automatically switches the interface
+language when it detects that the page being visited
+has a language code set in the database (via Special:PageLanguage).
+It also adds a helper link (in the page tools) to set this page's
+content language via the Special page.
+
+The extension requires the `$wgPageLanguageUseDB` global to be set to `true`.
+
+# Requirements
+
+* MediaWiki 1.35+
+* The extension requires the `$wgPageLanguageUseDB` global to be set to `true`
+
+# Setup
+
+* Clone to the `extensions` folder
+* Add `wfLoadExtension('PerPageLanguage')` to the settings file
+
+# Usage
+
+* Change some page language to the desired one via `Special:PageLanguage`
+* Visit the page and ensure your interface language is switched to the configured page language
+
+# Settings
+
+* `$wgPerPageLanguageIgnoreUserSetting` (default is `false`) - if to ignore the user's language
+set in their preferences. By default, the extension will only switch the interface language to
+the page language if it detects that the user language set matches the `$wgLanguageCode` setting.
+Setting this to `true` will make the extension ignore the user setting and *always* switch the
+interface language to the page language
+
---
+++ src/PerPageLanguageHooks.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace PerPageLanguage;
+
+use IContextSource;
+use Language;
+use MediaWiki\MediaWikiServices;
+use ReflectionException;
+use ReflectionMethod;
+use SkinTemplate;
+use SpecialPage;
+use User;
+
+class PerPageLanguageHooks {
+
+	/**
+	 * @param SkinTemplate $sktemplate
+	 * @param array &$links
+	 */
+	public static function onSkinTemplateNavigationUniversal( SkinTemplate $sktemplate, array &$links ) {
+		global $wgPageLanguageUseDB;
+
+		if ( !$wgPageLanguageUseDB ) {
+			// Nothing to do if the setting is disabled
+			return;
+		}
+
+		$context = $sktemplate->getContext();
+		$title = $context->getTitle();
+		$user = $context->getUser();
+
+		if ( !$title ) {
+			return;
+		}
+
+		if ( !$title->exists() ) {
+			return;
+		}
+
+		if ( $title->getNamespace() === NS_SPECIAL ) {
+			return;
+		}
+
+		if ( !MediaWikiServices::getInstance()->getPermissionManager()->userHasRight( $user, 'pagelang' ) ) {
+			return;
+		}
+
+		$links['actions']['pagelang'] = [
+			'class' => false,
+			'text' => $sktemplate->msg( 'perpagelanguage-label' )->text(),
+			'href' => SpecialPage::getTitleFor( 'PageLanguage' )->getLocalURL()
+					  . '/' . $context->getTitle()->getPrefixedText()
+		];
+	}
+
+	/**
+	 * @param User $user
+	 * @param string &$code
+	 * @param IContextSource $context
+	 *
+	 * @throws ReflectionException
+	 */
+	public static function onUserGetLanguageObject( User $user, string &$code, IContextSource $context ) {
+		global $wgPageLanguageUseDB, $wgPerPageLanguageIgnoreUserSetting, $wgLanguageCode;
+
+		if ( !$wgPageLanguageUseDB ) {
+			// Nothing to do if the setting is disabled
+			return;
+		}
+
+		$title = $context->getTitle();
+		if ( !$title || !$title->exists() ) {
+			// Do nothing for non existing titles
+			return;
+		}
+
+		// To avoid language recursion in RequestContext::getLanguage
+		// we always need to check if the page has language set in the database
+		// and otherwise just bow out quickly
+		// We need some magic here! *~~o-_*
+		try {
+			$method = new ReflectionMethod( $title, 'getDbPageLanguageCode' );
+		} catch ( ReflectionException $e ) {
+			// Do nothing on fail
+			return;
+		}
+		$method->setAccessible( true );
+		/** @var Language $pageLanguageDB */
+		$pageLanguageDB = $method->invoke( $title );
+		if ( !$pageLanguageDB ) {
+			// Do nothing if the page has no language set in the database
+			return;
+		}
+
+		if ( !$wgPerPageLanguageIgnoreUserSetting ) {
+			// If we want to respect the user preference on language
+			$userLanguage = MediaWikiServices::getInstance()
+				->getUserOptionsLookup()
+				->getOption( $user, 'language' );
+			if ( $userLanguage != $wgLanguageCode ) {
+				// If user did set language option to value different from
+				// the default one - do nothing
+				return;
+			}
+		}
+		// Forcefully set the interface language code to the page language
+		$code = $pageLanguageDB;
+	}
+
+}
